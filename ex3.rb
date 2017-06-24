bc8bb929e3023e23fda3c6ad332d9426f8a02e3b
Ryan Wetzelberger <rwetzelberger@videologygroup.com>
2017-06-24 18:26:00 -0400

    add example 3

---
+++ ex3.rb
@@ -0,0 +1,33 @@
+# prints string
+puts "I will now count my chickens:"
+
+# prints strings with mathmatical variable in string
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+# prints strings
+puts "Now I will count the eggs:"
+
+# prints calculation
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+#prints string without calculation due to not defining variable
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+#prints result of condition
+puts 3 + 2 < 5 - 7
+
+# prints strings with mathmatical variable in string
+puts "What is 3+2? #{3 + 2}"
+puts "What is 5-7? #{5 - 7}"
+
+# prints string
+puts "Oh, that's why it's false."
+
+# prints string
+puts "How about some more."
+
+# prints strings with mathmatical variable in string
+puts "Is it greater? #{5> -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
