59389cfab145a990e280c74286ca0abe14deb73a
Ryan Wetzelberger <rwetzelberger@videologygroup.com>
2017-06-25 13:43:12 -0400

    add ex6

---
+++ ex6.rb
@@ -0,0 +1,30 @@
+# define types_of_people
+types_of_people = 10
+
+# define x, binary, do_not, and y
+x = "there are #{types_of_people} types of people."
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+# outputs x and y
+puts x
+puts y
+
+# outputs strings using x and y
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+# sets variable with condition, sets variable with string using variable with condition
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+# outputs string variable
+puts joke_evaluation
+
+define two string variable
+w = "This is the left side of..."
+e = "a string with a right side."
+
+# outputs two variables
+puts w + e
