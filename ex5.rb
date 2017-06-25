2dd9c935db39e5172ce532742bef54e3e66f21ed
Ryan Wetzelberger <rwetzelberger@videologygroup.com>
2017-06-25 13:02:13 -0400

    add ex5

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+name = 'Ryan Wetzelberger'
+age = 28
+height = 70 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Black'
+
+puts "let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, #{weight} I get #{age + height + weight}."
