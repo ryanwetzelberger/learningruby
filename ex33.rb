da5743467ae77b07d9c50fc5d67f442ecbd4ff89
Ryan Wetzelberger <rwetzelberger@videologygroup.com>
2017-08-01 15:45:50 -0400

    modifying loop to be included in function

--- ex33.rb
+++ ex33.rb
@@ -1,14 +1,19 @@
-i = 0
-numbers = []
+def loop(test)
+  i = 0
+  numbers = []
 
-while i < 6
-  puts "At the top i is #{i}"
-  numbers.push(i)
+  while i < test
+    puts "At the top i is #{i}"
+    numbers.push(i)
 
-  i += 1
-  puts "Numbers now: ", numbers
-  puts "At the bottom i is #{i}"
+    i += 1
+    puts "Numbers now: ", numbers
+    puts "At the bottom i is #{i}"
+  end
+  return numbers
 end
+
+numbers = loop(6)
 
 puts "The numbers: "
 
